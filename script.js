2de13ff074b3151b277b2367e888efa272fe3a58
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-12 13:15:30 +0600

    update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
 //         console.log(sum + '+' + i + '=' + (sum+i));
 //         sum += i;
 //     }
-    
+
 // } 
 
 // 1
@@ -32,37 +32,37 @@
 // 1 2 3 4 5 6
 // nestedloop 
 
-let n = 20 
-for ( let i = 1 ; i <= n ; i++ ){
-    let result = '' ;
-    for( let j = 1 ; j <= i ; j++){
-        result += j + ' ' ;
+let n = 20
+for (let i = 1; i <= n; i++) {
+    let result = '';
+    for (let j = 1; j <= i; j++) {
+        result += j + ' ';
     }
     console.log(result);
 }
 
 // Sum  
 
-// let x = [ 100,200,300,400,500,600,700,800,900,1000,1,5,7]
-// let leng = x.length ;
-// let sum = 0 
-// for (let i = 0; i < leng; i++ ){
-//     if( x [i]% 2 === 1){
-//         sum += x[i];
-//     }
-// }
-// console.log(sum);
+let x = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1, 5, 7]
+let leng = x.length;
+let sum = 0
+for (let i = 0; i < leng; i++) {
+    if (x[i] % 2 === 1) {
+        sum += x[i];
+    }
+}
+console.log(sum);
 
-let arr =[
-    [30,40,20,30],
-    [40,38,48,50],
-    [30,48,50,44],
-    [45,49,50,40]
+let arr = [
+    [30, 40, 20, 30],
+    [40, 38, 48, 50],
+    [30, 48, 50, 44],
+    [45, 49, 50, 40]
 ]
 // console.log(arr[1]) ;
 
-for ( let i = 0 ; i < arr.length; i++){
-    for ( let j = 0; j < arr[i].length ; j++){
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
         console.log('Element ' + i + ' : ' + arr[i][j])
     }
 }
